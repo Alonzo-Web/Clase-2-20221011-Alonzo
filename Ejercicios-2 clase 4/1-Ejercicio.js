0151ed66ab8a52606abca4c500a34b76bdaf68b3
Alonzo-Web <112197072+Alonzo-Web@users.noreply.github.com>
2022-10-19 15:55:41 -0400

    Ejercicio

--- Ejercicios-2 clase 4/1-Ejercicio.js
+++ Ejercicios-2 clase 4/1-Ejercicio.js
@@ -1,1 +1,19 @@
 //Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a.
+const prompt = require('prompt');
+
+prompt.start()
+
+console.log("Escribir una frase");
+
+prompt.get(["valor"], function (error, resultado) {
+    let conteoLetraA = 0;
+    const valor = resultado.valor;
+    
+    for (let letra = 0; letra < valor.length; letra++) {
+        if (valor[letra] === "a") {
+            conteoLetraA = conteoLetraA + 1;
+        }
+    }
+
+    console.log("La letra 'a' aparece " + conteoLetraA + " de veces")
+});
