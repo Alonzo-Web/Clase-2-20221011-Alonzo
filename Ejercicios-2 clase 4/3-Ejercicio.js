0621cfde4780039410a9806a060c4d0b0f4d891a
Alonzo-Web <112197072+Alonzo-Web@users.noreply.github.com>
2022-10-20 16:01:31 -0400

    ejercicios

--- Ejercicios-2 clase 4/3-Ejercicio.js
+++ Ejercicios-2 clase 4/3-Ejercicio.js
@@ -1,1 +1,17 @@
 //Escribe un programa que pida una frase y escriba cuántas de las letras que tiene son vocales.
+
+const prompt = require('prompt');
+
+    prompt.start();
+
+    console.log('Escriba una frase');
+prompt.get(['frase'],function(error,resultado) {
+    let frase = resultado.frase;
+    let conteoVocales = +0;
+
+    for(let letras = 0; letras < frase.length; letras++) {
+        if(frase[letras] === 'a'){
+            console.log("Esta frase tiene",+conteoVocales, frase[letras])
+        }
+    }
+});  // proceso
