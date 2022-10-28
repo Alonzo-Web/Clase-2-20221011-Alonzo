6a2c93243d0d522d8be8334dbc6000716c682a2a
Alonzo-Web <alonzo0109@hotmail.com>
2022-10-27 22:29:13 -0400

    Ejercicios

--- Ejercicios-3 clase 6/index.js
+++ Ejercicios-3 clase 6/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
-/* const { dirname } = require('node:path/posix'); */
-const app = express()
 
-/* app.get('/', function (req, res) {
-  res.send('Hello World')
-}) */
+const app = express()
 
 app.use(express.static(__dirname + '/public'));
 
