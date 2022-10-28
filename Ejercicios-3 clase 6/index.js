cc187a9bc006a2991e08a6f1614b0d575efce856
Alonzo-Web <alonzo0109@hotmail.com>
2022-10-27 22:26:27 -0400

    Ejercicios

--- Ejercicios-3 clase 6/index.js
+++ Ejercicios-3 clase 6/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const { dirname } = require('node:path/posix');
+/* const { dirname } = require('node:path/posix'); */
 const app = express()
 
 /* app.get('/', function (req, res) {
   res.send('Hello World')
 }) */
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/vistas/index.html');
-  })
+    res.sendFile(__dirname + '/public/index.html');
+  });
+  
 
 app.listen(3000)
 console.log("Servidor web corriendo: http://localhost:3000")
